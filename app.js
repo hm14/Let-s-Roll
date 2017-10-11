c2b9e4f5fd283778300e2ccba88e169bab371350
Hannanah Mobashir <hanana.m@gmail.com>
2017-10-11 11:49:11 -0400

    feat: read/alter DOM elements

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, currentPlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+// 0 for first player, 1 for second player
+currentPlayer = 0;
+// randomly generate a number between 1 and 6 inclusive
+dice = Math.floor(Math.random()*6) + 1;
+
+// select element with id current-0 or current-1 with document.querySelector('#score-0')
+// read or change text of selected element using textContent
+
+// set value
+document.querySelector('#current-1').textContent = dice;
+
+// use innerHTML to pass html as a string from js
+document.querySelector('#current-' + currentPlayer).innerHTML = '<em>' + dice + '</em>';
+dice = Math.floor(Math.random()*6) + 1;
+
+// get value
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+// hide element
+document.querySelector('.dice').style.display = 'none';
